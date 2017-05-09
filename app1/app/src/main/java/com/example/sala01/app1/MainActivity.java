66694e7f3c62ba48b17453a1143d9ddd58f4b5b7
Matheus Lisboa de Barros <matheuslbarros@gmail.com>
2017-05-09 09:07:41 -0300

    Finish the app

--- app1/app/src/main/java/com/example/sala01/app1/MainActivity.java
+++ app1/app/src/main/java/com/example/sala01/app1/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.sala01.app1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        ArrayList<String> data = new ArrayList<>();
+        data.add("todo 1");
+        data.add("todo 2");
+
+        // ListView listaa = (ListView) R.id.listaa;
+        GridLayout lista = (GridLayout) R.id.lista;
+
+        for(String todo : data) {
+            TextView a = new TextView(getBaseContext());
+            a.setText(todo);
+            lista.addView(a);
+        }
+        */
     }
 }

23ecd225520f56a7bedbab3a3c6a8afb92f877c9
Matheus Lisboa de Barros <matheuslbarros@gmail.com>
2017-05-12 17:05:01 -0300

    App aula 5

--- app5/app/src/main/java/com/example/sala01/application5/PostAdapter.java
+++ app5/app/src/main/java/com/example/sala01/application5/PostAdapter.java
@@ -1,7 +1,6 @@
 package com.example.sala01.application5;
 
 import android.content.Context;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
